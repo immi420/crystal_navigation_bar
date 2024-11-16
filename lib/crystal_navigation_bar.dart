2307003b1c6f1b07b90b867f52871ced4183ab52
Imtiaz Ahmad <imtiazjan10@gmail.com>
2024-11-16 13:26:08 +0500

    Bug Fixed

--- lib/crystal_navigation_bar.dart
+++ lib/crystal_navigation_bar.dart
@@ -1,5 +1,5 @@
 library crystal_navigation_bar;
 
-export 'src/CrystalNavigationBarItem.dart';
-export 'src/NavBars.dart';
 export 'src/body.dart';
+export 'src/crystal_navigation_bar_item.dart';
+export 'src/nav_bars.dart';
